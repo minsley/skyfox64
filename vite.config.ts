e02a03259e86049e170d28e6bac770552015b228
Matt Insley <insley.matthew@gmail.com>
2025-08-04 02:26:20 -0700

    vite bs

--- vite.config.ts
+++ vite.config.ts
@@ -4,18 +4,4 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      // Proxy WebSocket upgrade requests to the Node.js server
-      '/': {
-        target: 'http://localhost:3000',
-        ws: true,
-        configure: (proxy) => {
-          proxy.on('upgrade', () => {
-            console.log('WebSocket upgrade request proxied to Node.js server');
-          });
-        },
-      },
-    },
-  },
 })
